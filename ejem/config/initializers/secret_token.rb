d132c989f8f632648965849b510edfa4e742a4d8
rosdaly cadena <rfcs88@hotmail.com>
2015-07-08 18:46:16 -0300

    Proyecto inicial

---
+++ ejem/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ejem::Application.config.secret_token = '530110875b38b99b71ea928cffba17386299bf6c58f20620aa98b7561a3bd15a0f50abb196b26c245b5aa7514d1257d892b9c0a56a57025721ff40b4aaa6d3cf'
